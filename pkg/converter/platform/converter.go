bc40a926e5147c4194b7e075ab8e840864660d3e
Zhao KK <kikakkz@hotmail.com>
2022-10-27 09:22:28 +0000

    Add good id to platform account

--- pkg/converter/platform/converter.go
+++ pkg/converter/platform/converter.go
@@ -2,7 +2,6 @@ package platform
 
 import (
 	"github.com/NpoolPlatform/account-manager/pkg/db/ent"
-	uuid1 "github.com/NpoolPlatform/go-service-framework/pkg/const/uuid"
 	account "github.com/NpoolPlatform/message/npool/account/mgr/v1/account"
 	npool "github.com/NpoolPlatform/message/npool/account/mgr/v1/platform"
 )
@@ -12,17 +11,11 @@ func Ent2Grpc(row *ent.Platform) *npool.Account {
 		return nil
 	}
 
-	var goodID *string
-	if row.GoodID.String() != uuid1.InvalidUUIDStr {
-		_str := row.GoodID.String()
-		goodID = &_str
-	}
-
 	return &npool.Account{
 		ID:        row.ID.String(),
 		AccountID: row.AccountID.String(),
 		UsedFor:   account.AccountUsedFor(account.AccountUsedFor_value[row.UsedFor]),
-		GoodID:    goodID,
+		GoodID:    row.GoodID.String(),
 		Backup:    row.Backup,
 		CreatedAt: row.CreatedAt,
 	}
